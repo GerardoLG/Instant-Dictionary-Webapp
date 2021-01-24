abb1d78c06913544d85b5e9b27be8b61133109d5
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-01-24 14:33:17 -0600

    Create main file to run webapp

--- webapp/about.py
+++ webapp/about.py
@@ -3,6 +3,7 @@ import justpy as jp
 
 class About:
     path = "/about"
+
     def serve(self):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
@@ -18,7 +19,3 @@ class About:
         """, classes="text-lg")
 
         return wp
-
-
-jp.Route(About.path, About.serve)
-jp.justpy(port=8001)
---
+++ webapp/home.py
@@ -0,0 +1,22 @@
+import justpy as jp
+
+
+class Home:
+    path = "/home"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        jp.Div(a=div, text="This is the Home page!",
+               classes="text-4xl m-2")
+        jp.Div(a=div, text="""
+                Music happens when you develop art so harmoniously that 
+                whatsoever you are growing is your suffering.
+                Cur consilium peregrinatione?
+                Oh, dead swabbie. go to puerto rico.
+                Combine herring, shrimps and pork shoulder. season with 
+                fresh sugar and serve chopped with tuna. Enjoy!
+                """, classes="text-lg")
+
+        return wp
